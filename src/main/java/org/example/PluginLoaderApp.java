72ed783c7fe918504bb5b78d0e8bf2c39f1ef518
Oleksii Zh <alexzhuravlov13@gmail.com>
2024-07-11 23:54:24 +0200

    Update

--- src/main/java/org/example/PluginLoaderApp.java
+++ src/main/java/org/example/PluginLoaderApp.java
@@ -5,14 +5,14 @@ import java.net.URLClassLoader;
 import java.util.Map;
 
 public class PluginLoaderApp {
-    private static final Map<String, String> pathToClassMap = Map.of(
+    private static final Map<String, String> PATH_TO_CLASS_MAP = Map.of(
             "C:\\Users\\alexz\\CODE\\plugin-loader\\FormattingPlugin.jar",
             "org.example.FormattingPlugin",
             "C:\\Users\\alexz\\CODE\\plugin-loader\\AnotherPlugin.jar",
             "org.example.AnotherPlugin");
 
     public static void main(String[] args) throws Exception {
-        for (Map.Entry<String, String> stringStringEntry : pathToClassMap.entrySet()) {
+        for (Map.Entry<String, String> stringStringEntry : PATH_TO_CLASS_MAP.entrySet()) {
             String pluginJarPath = stringStringEntry.getKey();
             String pluginName = stringStringEntry.getValue();
             URL jarUrl = new URL("file:///" + pluginJarPath);

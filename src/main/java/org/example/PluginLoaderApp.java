e5004a6e8a2595457f267474e08a8c1ead6c717b
Oleksii Zh <alexzhuravlov13@gmail.com>
2024-07-11 23:41:10 +0200

    Update

---
+++ src/main/java/org/example/PluginLoaderApp.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Map;
+
+public class PluginLoaderApp {
+    private static final Map<String, String> pathToClassMap = Map.of(
+            "C:\\Users\\alexz\\CODE\\plugin-loader\\FormattingPlugin.jar",
+            "org.example.FormattingPlugin",
+            "C:\\Users\\alexz\\CODE\\plugin-loader\\AnotherPlugin.jar",
+            "org.example.AnotherPlugin");
+
+    public static void main(String[] args) throws Exception {
+        for (Map.Entry<String, String> stringStringEntry : pathToClassMap.entrySet()) {
+            String pluginJarPath = stringStringEntry.getKey();
+            String pluginName = stringStringEntry.getValue();
+            URL jarUrl = new URL("file:///" + pluginJarPath);
+            try (URLClassLoader classLoader = new URLClassLoader(new URL[]{jarUrl})) {
+                Class<?> formattedClass = classLoader.loadClass(pluginName);
+                Plugin formattingPlugin = (Plugin) formattedClass.getDeclaredConstructor().newInstance();
+                formattingPlugin.execute();
+            } catch (ClassNotFoundException e) {
+                System.out.println(pluginName + " not found");
+            }
+        }
+    }
+}
